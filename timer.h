432b07602eb2af0051761d94726ec0fe7c7db051
Woocheck <34909593+Woocheck@users.noreply.github.com>
2019-06-01 21:44:01 +0200

    Time counter library.

---
+++ timer.h
@@ -0,0 +1,38 @@
+#ifndef __TIMER_H
+#define __TIMER_H
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+
+class timer
+{
+    private:
+        std::string id;
+        std::chrono::time_point<std::chrono::steady_clock> timerStart;
+        std::chrono::time_point<std::chrono::steady_clock> timerStop;
+        std::chrono::duration<double> duration;
+
+    public:
+        timer();
+        timer(const std::string& s): id(s){};
+
+        void start()
+        {
+            timerStart = (std::chrono::steady_clock::now());
+        }
+
+        void stop()
+        {
+            timerStop = (std::chrono::steady_clock::now());
+            duration = timerStop - timerStart;
+        }
+
+        std::chrono::duration<double> getDuration()
+        {
+            return duration;
+        }
+};
+
+#endif
