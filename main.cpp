4810913a1fcf207622902a5340270ab46f5e3df0
Woocheck <woocheck@op.pl>
2019-06-02 18:31:57 +0200

    Reordered main.cpp.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 #include <map>
 #include "./personContainer/person.h"
 #include "./timer/timer.h"
-#include "./functions/insertElement.h"
+#include "./mapFunctions/findElement.h"
+#include "./mapFunctions/makeMap.h"
+#include "./mapFunctions/insertElement.h"
+#include "./mapFunctions/deleteElement.h"
+
+
 
 
 std::vector<person::Person> preparePeopleList( const long elementsNumber)
@@ -17,23 +22,33 @@ std::vector<person::Person> preparePeopleList( const long elementsNumber)
     return peopleVector;
 }
 
-
+template<class Map>
+void printMap( const Map& mapToPrint )
+{
+    for(auto person : mapToPrint)
+        {
+            std::cout << person.first << " - " << person.second.name << " ";
+            std::cout << person.second.surname << " age: " << person.second.age;
+            std::cout << " " << person.second.city << " PESEL: " << person.second.pesel << std::endl;
+        }
+}
 
 int main(int argc, char ** argv)
 {
-   {
-        std::vector<person::Person> listOfPersons {};
-        listOfPersons = preparePeopleList(1000);
+   
+    std::vector<person::Person> listOfPersons {};
+    listOfPersons = preparePeopleList(1000);
 
-        std::vector<Timer> timers;
-        std::vector<long long> ids;
-        
-        for(auto person : listOfPersons)
-        {
-            std::cout << person << std::endl;
-        }
-    }
-    {
+    std::map<std::size_t, person::Person> map {};
+    std::multimap<std::size_t, person::Person> multimap {};
+
     
-    }
+    std::vector<DurationTime> testsResults;
+    std::vector<long long> ids;
+    
+    
+    std::chrono::duration<double> x = testMakeMapOfPersons(map,listOfPersons, 100);
+    printMap( map );
+
+    
 }
