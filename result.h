6da892f6173c15a60aebb9f774aa0532d80d92af
Woocheck <woocheck@op.pl>
2019-06-08 22:09:54 +0200

    print function added.

--- result.h
+++ result.h
@@ -2,6 +2,7 @@
 #define _RESULTS_H_
 
 #include <vector>
+#include <iostream>
 #include "./timer/timer.h"
 
 struct ResultContainer
@@ -10,11 +11,23 @@ struct ResultContainer
 
     inline ResultContainer& operator+=( const ResultContainer& newResults)
     {
-        for( auto result : newResults.results ) results.push_back( result );
-        //results.emplace( results.end(), newResults.results.begin(), newResults.results.end() );
+        for( auto result : newResults.results ) 
+            results.push_back( result );
+        return *this;
     }
     ResultContainer() = default;
     ResultContainer(std::vector<DurationTime>& result ): results( result ){};
+
+    void print()
+    {
+        std::for_each(std::begin( results ), std::end( results ), [](DurationTime test){
+            std::cout << test.containerType << " - " ;
+            std::cout << test.numberElementsInContainer << " elements ";
+            std::cout << " duration: " << test.duration.count() << std::endl;
+        });
+    }
 };
+
+
 
 #endif 
