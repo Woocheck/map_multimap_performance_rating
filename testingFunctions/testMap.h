af397176a103bc5dd745eeeefac8861932f7e3b2
Woocheck <woocheck@op.pl>
2019-06-08 22:13:03 +0200

    TestMap fixed and working correctly.

--- testingFunctions/testMap.h
+++ testingFunctions/testMap.h
@@ -17,25 +17,24 @@ template<class Map>
 ResultContainer testMapCreate( const std::string& container, Map& map,
                                const std::vector<person::Person>& listOfPersons)
 {
-    ResultContainer results;
+    ResultContainer result;
     
     for(std::size_t elementsNumber {1}; elementsNumber <= listOfPersons.size(); elementsNumber *= 10 )
     {
         map.clear();
         DurationTime singleTest( container );
+
         singleTest.testType = TypesOfTests::makeColection;
+        singleTest.containerType = container;
         singleTest.numberElementsInContainer = elementsNumber;
         singleTest.duration = makeMapOfPersons( map, listOfPersons, elementsNumber );
-        
-        results.results.push_back( singleTest );
-        std::cout << map.size() << " - map " << std::endl;
-       
+        result.results.push_back( singleTest );  
     }
 
   
 
    
-    return results;
+    return result;
 }
 
 template<class Map>
@@ -49,14 +48,35 @@ ResultContainer testMapInsert( const std::string& container, Map& map,
     {
         map.clear();
         makeMapOfPersons( map, listOfPersons, elementsNumber );
-        std::cout << map.size() << " - before insert " << std::endl;
         DurationTime singleTest( container );
+        
         singleTest.testType = TypesOfTests::insertElement;
         singleTest.numberElementsInContainer = elementsNumber;
-        singleTest.duration = insertElements( map, smallListOfPersons);
+        singleTest.duration = insertElements( map, smallListOfPersons, elementsNumber);
 
         results.results.push_back( singleTest );
-        std::cout << map.size() << " - after " << std::endl;
+    }
+    return results;
+}
+
+template<class Map>
+ResultContainer testFindElement( const std::string& container, Map& map,
+                               const std::vector<person::Person>& listOfPersons,
+                               const std::vector<person::Person>& smallListOfPersons )
+{
+    ResultContainer results;
+    
+    for(std::size_t elementsNumber {1}; elementsNumber <= listOfPersons.size(); elementsNumber *= 10 )
+    {
+        map.clear();
+        makeMapOfPersons( map, listOfPersons, elementsNumber );
+        DurationTime singleTest( container );
+        
+        singleTest.testType = TypesOfTests::findElement;
+        singleTest.numberElementsInContainer = elementsNumber;
+        singleTest.duration = findElement( map, smallListOfPersons, elementsNumber);
+
+        results.results.push_back( singleTest );
     }
     return results;
 }
