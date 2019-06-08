20ed5eed3e9d5f3935fbd64496f054ce3ea2398b
Woocheck <woocheck@op.pl>
2019-06-08 22:13:42 +0200

    InserElement fixed and working correctly.

--- mapFunctions/insertElement.h
+++ mapFunctions/insertElement.h
@@ -6,30 +6,37 @@
 #include <unordered_map>
 #include <map>
 #include <algorithm>
+#include <random>
 #include "../personContainer/person.h"
 #include "../timer/timer.h"
 #include "makeMap.h"
 
+
 template <class Map>
-std::chrono::duration<double> insertElements( Map& map, 
-                                              const std::vector<person::Person>& personsToinsert ) 
+std::chrono::duration<double> insertElements( const Map& map, 
+                                              const std::vector<person::Person>& personsToinsert,
+                                              const std::size_t elementsNumber ) 
 {
     std::vector<Timer> listOfTimers;
-    std::size_t i {1};
-    for(auto person : personsToinsert)
-    {   
-        i++;
-        if( map.count( i ) > 0 ) i++;
-        Timer insertPeopleTimer( "Insert people id: " + i);
+    
+    
+    for ( int test { 0 }; test < 5 ; test++ )
+    {
+        Timer insertPeopleTimer( "Insert people");
+        Map singleTestMap = map;
         insertPeopleTimer.start();
 
-        map.insert( std::pair<std::size_t, person::Person>( map.size()/2 + i, person ));
+            for( std::size_t i {0} ; i < personsToinsert.size() ; i ++ )
+            {   
+                    singleTestMap.insert( std::make_pair( personsToinsert.at( i ) , std::rand() ) );
+            }
 
         insertPeopleTimer.stop();
+
         listOfTimers.push_back( insertPeopleTimer );
-    }
+    };
+    discardExtremeValues ( listOfTimers );
     
-    sortTimers ( listOfTimers );
     return averageTimeValue( listOfTimers );
 }
 
